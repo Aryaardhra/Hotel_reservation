f820e3f48e56443b114149597c504f0ef91a92df
Aryaardhra <ardhra455@gmail.com>
2023-11-07 16:10:00 +0530

    server

---
+++ server/routes/hotelsRoute.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { countByCity, countByType, createHotel, deleteHotel, getAllHotel, getHotel, updateHotel } from "../controller/hotelController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+// create
+
+router.post("/", verifyAdmin, createHotel );
+
+// update
+
+router.put("/:id",verifyAdmin, updateHotel);
+
+// delete
+
+router.delete("/:id/:hotelid", verifyAdmin, deleteHotel);
+
+// get single hotel
+
+router.get("/find/:id", getHotel);
+
+// get all hotels
+
+router.get("/", getAllHotel);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+
+export default router;
